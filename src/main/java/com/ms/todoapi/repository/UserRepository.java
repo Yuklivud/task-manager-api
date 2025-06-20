ef7bc09de9a1f259bcd794ac4ed5ec565d82904c
Maksym Shkarbun <suprime28@yahoo.com>
2025-06-20 17:40:50 +0300

    feat: add User Repository

---
+++ src/main/java/com/ms/todoapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ms.todoapi.repository;
+
+import com.ms.todoapi.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
