48b4befd8c4551f1d9c6802db93e58918029c393
Maksym Shkarbun <suprime28@yahoo.com>
2025-06-25 01:07:36 +0300

    feat(UserService): add method to map User entity to UserDto

--- src/main/java/com/ms/todoapi/service/UserService.java
+++ src/main/java/com/ms/todoapi/service/UserService.java
@@ -1,8 +1,25 @@
 package com.ms.todoapi.service;
 
+import com.ms.todoapi.dto.UserDto;
+import com.ms.todoapi.model.entity.Role;
+import com.ms.todoapi.model.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
+    public UserDto mapToDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getEmail(),
+                user.getIsActive(),
+                user.getCreatedAt(),
+                user.getRoles()
+                        .stream()
+                        .map(Role::getRole)
+                        .collect(Collectors.toSet())
+        );
+    }
 }
