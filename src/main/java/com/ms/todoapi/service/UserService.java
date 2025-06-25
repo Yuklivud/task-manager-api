6ccfcacf794280d2f797c4430e361b7b52b2aeb9
Maksym Shkarbun <suprime28@yahoo.com>
2025-06-25 18:52:46 +0300

    feat(service): add find-by-id method

--- src/main/java/com/ms/todoapi/service/UserService.java
+++ src/main/java/com/ms/todoapi/service/UserService.java
@@ -3,12 +3,22 @@ package com.ms.todoapi.service;
 import com.ms.todoapi.dto.UserDto;
 import com.ms.todoapi.model.entity.Role;
 import com.ms.todoapi.model.entity.User;
+import com.ms.todoapi.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public UserDto mapToDto(User user) {
         return new UserDto(
@@ -21,5 +31,9 @@ public class UserService {
                         .map(Role::getRole)
                         .collect(Collectors.toSet())
         );
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User with id " + id + " not found"));
     }
 }
