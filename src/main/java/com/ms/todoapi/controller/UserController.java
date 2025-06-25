8fa561e40312383ff90205d72c8e7634c5371687
Maksym Shkarbun <suprime28@yahoo.com>
2025-06-25 19:06:48 +0300

    feat(user): add GET endpoint to retrieve user by ID

--- src/main/java/com/ms/todoapi/controller/UserController.java
+++ src/main/java/com/ms/todoapi/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.ms.todoapi.controller;
 
+import com.ms.todoapi.dto.UserDto;
+import com.ms.todoapi.model.entity.User;
 import com.ms.todoapi.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +21,10 @@ public class UserController {
 
     public final UserService userService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        UserDto dto = userService.mapToDto(user);
+        return ResponseEntity.ok(dto);
+    }
 }
