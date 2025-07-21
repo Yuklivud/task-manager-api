b1166aaee9760b796062ffae74a278199548e2a2
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-22 02:44:41 +0300

    refactor(service): Removed manual DTO mapper

--- src/main/java/com/ms/todoapi/service/UserService.java
+++ src/main/java/com/ms/todoapi/service/UserService.java
@@ -1,14 +1,11 @@
 package com.ms.todoapi.service;
 
-import com.ms.todoapi.dto.UserDto;
-import com.ms.todoapi.model.entity.Role;
 import com.ms.todoapi.model.entity.User;
 import com.ms.todoapi.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -18,19 +15,6 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    public UserDto mapToDto(User user) {
-        return new UserDto(
-                user.getId(),
-                user.getEmail(),
-                user.getIsActive(),
-                user.getCreatedAt(),
-                user.getRoles()
-                        .stream()
-                        .map(Role::getRole)
-                        .collect(Collectors.toSet())
-        );
     }
 
     public User findById(Long id) {
