1c0a7b5e377606434e22007de5bf42631b1c2fe8
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-23 13:14:10 +0300

    feat(mapper): add method to convert Role entities to role name string

--- src/main/java/com/ms/todoapi/mapper/UserMapper.java
+++ src/main/java/com/ms/todoapi/mapper/UserMapper.java
@@ -1,14 +1,30 @@
 package com.ms.todoapi.mapper;
 
 import com.ms.todoapi.dto.UserResponse;
+import com.ms.todoapi.model.entity.Role;
 import com.ms.todoapi.model.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
+
+    @Mapping(target = "roles", source = "roles", qualifiedByName = "mapRolesToNames")
     UserResponse userToUserResponse(User user);
+
     List<UserResponse> usersToUserResponses(List<User> users);
+
+    @Named("mapRolesToNames")
+    static Set<String> mapRolesToNames(Set<Role> roles){
+        if (roles == null) return Set.of();
+        return roles.stream()
+                .map(Role::getRole)
+                .collect(Collectors.toSet());
+    }
 }
