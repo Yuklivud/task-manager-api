51cfecba588342f783f2d527fcf53bad85d2d181
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-23 13:29:48 +0300

    refactor: remove Lombok annotation and add explicit constructor

--- src/main/java/com/ms/todoapi/controller/UserController.java
+++ src/main/java/com/ms/todoapi/controller/UserController.java
@@ -6,7 +6,6 @@ import com.ms.todoapi.model.entity.User;
 import com.ms.todoapi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/users")
-@RequiredArgsConstructor
 @Tag(name = "User Controller", description = "User-related operation")
 public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> findByID(@PathVariable Long id) {
