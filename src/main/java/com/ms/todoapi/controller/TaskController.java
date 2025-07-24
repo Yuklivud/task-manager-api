607095e82e34516be0f5be40676fdf7bdfda3ae9
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-24 21:33:45 +0300

    feat(controller): add task controller

---
+++ src/main/java/com/ms/todoapi/controller/TaskController.java
@@ -0,0 +1,20 @@
+package com.ms.todoapi.controller;
+
+import com.ms.todoapi.mapper.TaskMapper;
+import com.ms.todoapi.service.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+}
