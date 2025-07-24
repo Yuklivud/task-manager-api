9bd741c78805f73dbc0e6468f3bc1ac33b5dea4b
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-24 21:17:18 +0300

    feat(service): add task service

---
+++ src/main/java/com/ms/todoapi/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.ms.todoapi.service;
+
+import com.ms.todoapi.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+}
