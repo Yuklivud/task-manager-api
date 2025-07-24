36a1b2c1456fa5ec23342519f53a311a663a5e4f
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-24 21:37:08 +0300

    feat(task-controller): implement get by id endpoint

--- src/main/java/com/ms/todoapi/controller/TaskController.java
+++ src/main/java/com/ms/todoapi/controller/TaskController.java
@@ -1,7 +1,12 @@
 package com.ms.todoapi.controller;
 
+import com.ms.todoapi.dto.TaskResponse;
 import com.ms.todoapi.mapper.TaskMapper;
+import com.ms.todoapi.model.entity.Task;
 import com.ms.todoapi.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +20,13 @@ public class TaskController {
     public TaskController(TaskService taskService, TaskMapper taskMapper) {
         this.taskService = taskService;
         this.taskMapper = taskMapper;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponse> findById(@PathVariable Long id) {
+        Task task = taskService.findById(id);
+        TaskResponse dto = taskMapper.taskToTaskResponse(task);
+        return ResponseEntity.ok(dto);
     }
 
 }
