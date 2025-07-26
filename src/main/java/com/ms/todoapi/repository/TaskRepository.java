758dd013a44bb7ca500e1645b555d1d778331599
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-26 22:21:28 +0300

    feat(task-repository): add find by id and user method

--- src/main/java/com/ms/todoapi/repository/TaskRepository.java
+++ src/main/java/com/ms/todoapi/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.ms.todoapi.repository;
 
 import com.ms.todoapi.model.entity.Task;
+import com.ms.todoapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByIdAndUser(Long id, User user);
 }
