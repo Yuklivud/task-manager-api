aea8c06ec744f97ec32d426e4c4198c3f31df053
Maksym Shkarbun <suprime28@yahoo.com>
2025-07-28 01:09:17 +0300

    feat(role-repository): add role repository

---
+++ src/main/java/com/ms/todoapi/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.ms.todoapi.repository;
+
+import com.ms.todoapi.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
