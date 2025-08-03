7b681924798bcf30d3868d97b4c53127c46e4101
Maksym Shkarbun <suprime28@yahoo.com>
2025-08-03 21:10:13 +0300

    feat(auth-controller): implement sign up endpoint

--- src/main/java/com/ms/todoapi/controller/AuthController.java
+++ src/main/java/com/ms/todoapi/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.ms.todoapi.controller;
 
+import com.ms.todoapi.dto.AuthRequest;
+import com.ms.todoapi.dto.AuthResponse;
 import com.ms.todoapi.security.AuthenticationService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,5 +15,8 @@ public class AuthController {
         this.authenticationService = authenticationService;
     }
 
-
+    @PostMapping("/signup")
+    public AuthResponse signUp(@RequestBody @Valid AuthRequest authRequest) {
+        return authenticationService.signUp(authRequest);
+    }
 }
