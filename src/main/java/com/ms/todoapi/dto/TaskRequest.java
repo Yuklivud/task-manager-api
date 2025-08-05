7d96ee4ad0577bb078e6f0788d6ceb4a5e884c21
Maksym Shkarbun <suprime28@yahoo.com>
2025-08-06 00:02:56 +0300

    fix(dto): change LocalDate to LocalDateTime

--- src/main/java/com/ms/todoapi/dto/TaskRequest.java
+++ src/main/java/com/ms/todoapi/dto/TaskRequest.java
@@ -3,11 +3,11 @@ package com.ms.todoapi.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record TaskRequest(
     @NotBlank String title,
     @NotBlank String description,
-    @NotNull LocalDate deadline
+    @NotNull LocalDateTime deadline
 )
 {}
