631d35172f496ca673524351ae94cda0f95dc6af
Maksym Shkarbun <suprime28@yahoo.com>
2025-08-05 23:52:28 +0300

    fix(user-entity): change password length 50 -> 100

--- src/main/java/com/ms/todoapi/model/entity/User.java
+++ src/main/java/com/ms/todoapi/model/entity/User.java
@@ -23,7 +23,7 @@ public class User implements UserDetails {
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String password;
 
     @Column(name = "active", nullable = false)
